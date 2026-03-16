2acc398c7feea616a83c0c37051a2f6e605344e4
Skip <skip@usecurling.com>
2026-03-16 15:09:36 +0000

    feat: Add Help section with video tutorials and FAQs

--- supabase/functions/functions/index.ts
+++
@@ -1,7 +0,0 @@
-import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
-
-Deno.serve(async () => {
-  return new Response(JSON.stringify({ status: 'ok' }), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-})
