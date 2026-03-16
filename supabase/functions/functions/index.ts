a10305bf68df68e25f4cf0e8e0128df6676dd6f2
Skip <skip@usecurling.com>
2026-03-16 18:47:59 +0000

    Auto-commit before sync

--- supabase/functions/functions/index.ts
+++ supabase/functions/functions/index.ts
@@ -1,8 +1,11 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 
 Deno.serve(async (req: Request) => {
-  return new Response(JSON.stringify({ message: 'Success' }), {
-    headers: { 'Content-Type': 'application/json' },
-    status: 200,
-  })
+  return new Response(
+    JSON.stringify({ message: 'Success' }),
+    { 
+      headers: { 'Content-Type': 'application/json' },
+      status: 200 
+    }
+  )
 })
